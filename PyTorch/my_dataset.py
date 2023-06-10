6c6729ee61a04cfc6a35b50608003dd4b91fd5b2
Ebrahim Ghafourzadeh <90109369+E-Ghafour@users.noreply.github.com>
2023-06-10 03:56:04 +0330

    change the dataset structure to make it runable by cuda

--- PyTorch/my_dataset.py
+++ PyTorch/my_dataset.py
@@ -10,17 +10,20 @@ class Dataset_POS(Dataset):
         self.pad_len = pad_len
         with open(tokenize_data_path, 'rb') as ff:
             sents = pickle.load(ff)
-        self.tokens = [[word for word, _ in sent]for sent in sents]
-        self.labels = [[label for _, label in sent]for sent in sents]
+        self.tokens = [[word for word, _ in sent]for sent in sents[:sents_num]]
+        self.labels = [[label for _, label in sent]for sent in sents[:sents_num]]
+        
         self.label_to_id = self.__create_label_dict()
         self.vocab_to_id = word2vec.get_vocab_to_index()
         self.vocabs = word2vec.get_vocabs()
         pad_label_one_hot = [0] * (self.num_labels + 1)
         pad_label_one_hot[-1] = 1
         self.pad_label_one_hot = pad_label_one_hot
+        self.padded_x = [self.convert_x(sent) for sent in tqdm(self.tokens[:sents_num])]
+        self.padded_y = [self.convert_y(label) for label in tqdm(self.labels[:sents_num])]
 
     def __len__(self):
-        return len(self.tokenize_data)
+        return len(self.tokens)
 
     def __create_label_dict(self):
         flatten_label = list(itertools.chain(*self.labels))
@@ -34,17 +37,30 @@ class Dataset_POS(Dataset):
         one_hot[self.label_to_id[label]] = 1
         return one_hot
     
+    def convert_x(self, x):
+        x_indices = [self.vocab_to_id[token] if token in self.vocabs else len(self.vocabs) for token in x]
+        x_padded_indices = x_indices[:self.pad_len] + [len(self.vocabs)] * (self.pad_len - len(x_indices[:self.pad_len]))
+        return x_padded_indices
+
+    def convert_y(self, y):
+        y = self.convert_labels_to_onehot(y)
+        y_padded_indices = y[:self.pad_len] + [self.pad_label_one_hot] * (self.pad_len - len(y[:self.pad_len]))
+        return y_padded_indices
+    
     def convert_labels_to_onehot(self, labels: list):
         return [self.__convert_label_to_onehot(label) for label in labels]
         
     def __getitem__(self, idx):
-        x = self.tokens[idx]
-        y = self.labels[idx]
+        # x = self.tokens[idx]
+        # y = self.labels[idx]
 
-        x_indices = [self.vocab_to_id[token] if token in self.vocabs else 'unk' for token in x]
-        x_padded_indices = x_indices[:self.pad_len] + [0] * (self.pad_len - len(x_indices[:self.pad_len]))
+        # x_indices = [self.vocab_to_id[token] if token in self.vocabs else len(self.vocabs) for token in x]
+        # x_padded_indices = x_indices[:self.pad_len] + [len(self.vocabs)] * (self.pad_len - len(x_indices[:self.pad_len]))
 
-        y = self.convert_labels_to_onehot(y)
-        y_padded_indices = y[:self.pad_len] + [self.pad_label_one_hot] * (self.pad_len - len(y[:self.pad_len]))
+        # y = self.convert_labels_to_onehot(y)
+        # y_padded_indices = y[:self.pad_len] + [self.pad_label_one_hot] * (self.pad_len - len(y[:self.pad_len]))
 
-        return torch.tensor(x_padded_indices), torch.tensor(y_padded_indices)
+        x = self.padded_x[idx]
+        y = self.padded_y[idx]
+
+        return torch.tensor(x), torch.tensor(y).float()
